244240b7c3b42bcb518000eecaa99441bda16cea
Shay Wagner <srwagner916@gmail.com>
2021-05-29 15:08:22 -0500

    wrote route to get all categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,18 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
