107be915348727cfe6d013f643e4236f13aa753a
Shay Wagner <srwagner916@gmail.com>
2021-05-29 15:42:58 -0500

    wrote route to create a new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -47,6 +47,18 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  // expect req.body to look like
+  // {
+  //   category_name: bread
+  // }
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
