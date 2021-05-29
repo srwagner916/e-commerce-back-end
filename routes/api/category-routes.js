015b4f8bc8c3c04ee7e7d52d31a8fd5749cabb4c
Shay Wagner <srwagner916@gmail.com>
2021-05-29 15:55:42 -0500

    wrote route to delete a category by ID

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,7 +36,7 @@ router.get('/:id', (req, res) => {
   })
     .then(dbCategoryData => {
       if(!dbCategoryData) {
-        res.status(404).json({ message: 'No category with that ID found' });
+        res.status(404).json({ message: 'No category with that ID was found' });
         return;
       }
     })
@@ -75,7 +75,7 @@ router.put('/:id', (req, res) => {
   )
     .then(dbCategoryData => {
       if(!dbCategoryData) {
-        res.status(404).json({ message: 'No category with this ID found' })
+        res.status(404).json({ message: 'No category with this ID was found' })
         return;
       }
       res.json(dbCategoryData);
@@ -88,6 +88,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCategoryData => {
+      if(!dbCategoryData) {
+        res.status(404).json({ message: 'No category with this ID was found'})
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
